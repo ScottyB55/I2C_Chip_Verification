e1ba618b16f5df71fd6165ad95f559318a258608
srburne2 <srburne2@ncsu.edu>
2023-05-11 18:53:22 -0400

    commented i2c_agent

--- verification_ip/interface_packages/i2c_pkg/src/i2c_agent.svh
+++ verification_ip/interface_packages/i2c_pkg/src/i2c_agent.svh
@@ -1,13 +1,46 @@
 class i2c_agent extends ncsu_component#(.T(ncsu_transaction));
+/*
+A class used to represent an I2C Agent.
+
+This class is responsible for managing I2C communication in hardware.
+It handles operations such as setting configurations, building I2C components,
+connecting subscribers, running the monitor and sending transactions to 
+subscribers and the driver.
+
+Attributes
+----------
+configuration : i2c_configuration
+	The configuration settings for the I2C components
+i2c_cov_h : i2c_coverage
+	The I2C coverage handler
+drv_h : i2c_driver
+	The I2C driver handler
+mon_h : i2c_monitor
+	The I2C monitor handler
+subscribers : ncsu_component #(T) []
+	The subscribers of the I2C agent
+bus : virtual i2c_if
+	The interface for the I2C bus
+*/
   
-  i2c_configuration configuration;
-  i2c_coverage i2c_cov_h;
-  i2c_driver        drv_h;
-  i2c_monitor       mon_h;
-  ncsu_component #(T) subscribers[$];
-  
-  virtual i2c_if    bus;
+	i2c_configuration configuration;
+	i2c_coverage i2c_cov_h;
+	i2c_driver        drv_h;
+	i2c_monitor       mon_h;
+	ncsu_component #(T) subscribers[$];
+	virtual i2c_if    bus;
+
  	function new(string name = "", ncsu_component_base  parent = null); 
+        /*
+        Constructs all the necessary attributes for the i2c_agent object.
+
+        Parameters
+        ----------
+        name : string
+            The name of the I2C agent
+        parent : ncsu_component_base
+            The parent component of the I2C agent
+        */
     	super.new(name,parent);
 
     	if (!(ncsu_config_db#(virtual i2c_if)::get(get_full_name(), this.bus))) begin;
@@ -16,10 +49,22 @@ class i2c_agent extends ncsu_component#(.T(ncsu_transaction));
  	endfunction
 
 	function void set_configuration(i2c_configuration cfg);
+        /*
+        Sets the configuration for the I2C agent.
+
+        Parameters
+        ----------
+        cfg : i2c_configuration
+            The configuration to be set
+        */
 	    configuration = cfg;
 	endfunction
 
 	virtual function void build();
+        /*
+        Builds the I2C components: driver, monitor and coverage handler. 
+        Also connects the coverage handler as a subscriber.
+        */
 		// instantiating monitor
 		mon_h = new("mon_h",this);
 	    	mon_h.set_configuration(configuration);
@@ -40,21 +85,48 @@ class i2c_agent extends ncsu_component#(.T(ncsu_transaction));
 	endfunction
 
  	virtual task bl_put(T trans);
-    		drv_h.bl_put(trans);
+        /*
+        Sends the transaction to the driver.
+
+        Parameters
+        ----------
+        trans : T
+            The transaction to be sent
+        */
+    	drv_h.bl_put(trans);
   	endtask
 
   	virtual function void connect_subscriber(ncsu_component#(T) subscriber);
-    		subscribers.push_back(subscriber);
+        /*
+        Connects a subscriber to the I2C agent.
+
+        Parameters
+        ----------
+        subscriber : ncsu_component#(T)
+            The subscriber to be connected
+        */
+    	subscribers.push_back(subscriber);
   	endfunction
 
   	virtual task run();
-    		fork 
+        /*
+        Runs the monitor task asynchronously.
+        */
+    	fork 
 			mon_h.run(); 
 		join_none
   	endtask
 
 	virtual function void nb_put(T trans);
-    		foreach (subscribers[i]) subscribers[i].nb_put(trans);
+        /*
+        Sends the transaction to all the subscribers.
+
+        Parameters
+        ----------
+        trans : T
+            The transaction to be sent
+        */
+    	foreach (subscribers[i]) subscribers[i].nb_put(trans);
   	endfunction
 
 endclass
