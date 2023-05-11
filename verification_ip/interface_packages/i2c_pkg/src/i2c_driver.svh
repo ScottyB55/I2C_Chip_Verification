d445341fa1547e5d1ca37b8fbe433f12106ab4e7
srburne2 <srburne2@ncsu.edu>
2023-05-11 19:08:15 -0400

    commented the driver & monitor

--- verification_ip/interface_packages/i2c_pkg/src/i2c_driver.svh
+++ verification_ip/interface_packages/i2c_pkg/src/i2c_driver.svh
@@ -1,20 +1,62 @@
 class i2c_driver extends ncsu_component#(.T(ncsu_transaction));
+/*
+A class used to represent an I2C Driver.
 
-  function new(string name = "", ncsu_component_base  parent = null); 
-    super.new(name,parent);
-  endfunction
+This class is responsible for managing I2C transactions in hardware.
+It handles operations such as setting configurations and providing read data to the 
+I2C interface.
 
-  virtual i2c_if i2c_intf_h;
-  i2c_transaction i2c_trans_h;
-  i2c_configuration configuration;
+Attributes
+----------
+i2c_intf_h : virtual i2c_if
+    The interface handler for the I2C bus
+i2c_trans_h : i2c_transaction
+    The I2C transaction handler
+configuration : i2c_configuration
+    The configuration settings for the I2C driver
+*/
 
-  function void set_configuration(i2c_configuration cfg);
-    configuration = cfg;
-  endfunction
+    virtual i2c_if i2c_intf_h;
+    i2c_transaction i2c_trans_h;
+    i2c_configuration configuration;
 
-  virtual task bl_put(T trans);
-    $cast(i2c_trans_h,trans);
-    i2c_intf_h.provide_read_data(i2c_trans_h.data);
-  endtask
+    function new(string name = "", ncsu_component_base  parent = null);
+        /*
+        Constructs all the necessary attributes for the i2c_driver object.
+
+        Parameters
+        ----------
+        name : string
+            The name of the I2C driver
+        parent : ncsu_component_base
+            The parent component of the I2C driver
+        */
+        super.new(name,parent);
+    endfunction
+
+    function void set_configuration(i2c_configuration cfg);
+        /*
+        Sets the configuration for the I2C driver.
+
+        Parameters
+        ----------
+        cfg : i2c_configuration
+            The configuration to be set
+        */
+        configuration = cfg;
+    endfunction
+
+    virtual task bl_put(T trans);
+        /*
+        Sends the transaction to the I2C interface for reading data.
+
+        Parameters
+        ----------
+        trans : T
+            The transaction to be sent
+        */
+        $cast(i2c_trans_h,trans);
+        i2c_intf_h.provide_read_data(i2c_trans_h.data);
+    endtask
 
 endclass : i2c_driver
--- verification_ip/interface_packages/i2c_pkg/src/i2c_monitor.svh
+++ verification_ip/interface_packages/i2c_pkg/src/i2c_monitor.svh
@@ -1,31 +1,80 @@
 class i2c_monitor extends ncsu_component#(.T(ncsu_transaction));
-  i2c_configuration  configuration;
-  virtual i2c_if i2c_intf_h;
+    /*
+    A class used to represent an I2C Monitor.
 
-  i2c_transaction monitored_trans;
-  ncsu_component #(T) agent;
+    This class is responsible for monitoring I2C transactions on the bus.
+    It handles operations such as setting configurations, setting agent and running the monitoring task.
 
-  function new(string name = "", ncsu_component_base  parent = null); 
-    super.new(name,parent);
-  endfunction
+    Attributes
+    ----------
+    configuration : i2c_configuration
+        The configuration settings for the I2C monitor
+    i2c_intf_h : virtual i2c_if
+        The interface handler for the I2C bus
+    monitored_trans : i2c_transaction
+        The I2C transaction being monitored
+    agent : ncsu_component#(T)
+        The agent responsible for the I2C transaction
+    */
+    i2c_configuration  configuration;
+    virtual i2c_if i2c_intf_h;
 
-  function void set_configuration(i2c_configuration cfg);
-    configuration = cfg;
-  endfunction
+    i2c_transaction monitored_trans;
+    ncsu_component #(T) agent;
 
-  function void set_agent(ncsu_component#(T) agent);
-    this.agent = agent;
-  endfunction
-  
-  virtual task run ();
-      forever begin
-        monitored_trans = new("monitored transaction");
-        i2c_intf_h.monitor(monitored_trans.address,
-                    monitored_trans.rw,
-                    monitored_trans.data
-                    );
-        monitored_trans.num_bytes = monitored_trans.data.size();
-        agent.nb_put(monitored_trans);
-    end
-  endtask
+    function new(string name = "", ncsu_component_base  parent = null);
+        /*
+        Constructs all the necessary attributes for the i2c_monitor object.
+
+        Parameters
+        ----------
+        name : string
+            The name of the I2C monitor
+        parent : ncsu_component_base
+            The parent component of the I2C monitor
+        */
+        super.new(name,parent);
+    endfunction
+
+    function void set_configuration(i2c_configuration cfg);
+        /*
+        Sets the configuration for the I2C monitor.
+
+        Parameters
+        ----------
+        cfg : i2c_configuration
+            The configuration to be set
+        */
+        configuration = cfg;
+    endfunction
+
+    function void set_agent(ncsu_component#(T) agent);
+        /*
+        Sets the agent for the I2C monitor.
+
+        Parameters
+        ----------
+        agent : ncsu_component#(T)
+            The agent to be set
+        */
+        this.agent = agent;
+    endfunction
+
+    virtual task run ();
+        /*
+        Runs the I2C monitoring task.
+
+        The task continuously monitors the I2C transactions on the bus,
+        and sends the monitored transaction to the agent.
+        */
+        forever begin
+            monitored_trans = new("monitored transaction");
+            i2c_intf_h.monitor( monitored_trans.address,
+                                monitored_trans.rw,
+                                monitored_trans.data
+                                );
+            monitored_trans.num_bytes = monitored_trans.data.size();
+            agent.nb_put(monitored_trans);
+        end
+    endtask
 endclass
