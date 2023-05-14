42276eb7ad284bf9212ecdb3225470bd22f97a45
srburne2 <srburne2@ncsu.edu>
2023-05-13 22:06:57 -0400

    Understanding and commenting everything related to lab 1!

--- project_benches/lab_1/testbench/top.sv
+++ project_benches/lab_1/testbench/top.sv
@@ -1,19 +1,25 @@
 `timescale 1ns / 10ps
 
 module top();
+/*
+Top level module to instantiate an I2C multiple bus controller, 
+drive some basic signals to configure it, and monitor it.
+*/
 
-parameter int WB_ADDR_WIDTH = 2;
-parameter int WB_DATA_WIDTH = 8;
-parameter int NUM_I2C_BUSSES = 1;
+parameter int WB_ADDR_WIDTH = 2;    // Width of the Wishbone address bus
+parameter int WB_DATA_WIDTH = 8;    // Width of the Wishbone data bus
+parameter int NUM_I2C_BUSSES = 1;   // Number of I2C buses
 
+// Defining the enumeration for register offsets
+// See the I2C Multiple Bus Controller IP Core Specification PDF
 typedef enum logic  [1:0] {
-    CSR = 2'd0,
-    DPR = 2'd1,
-    CMDR = 2'd2,
-    FSMR = 2'd3
+    CSR = 2'd0, // Control/Status Register, Pg 18
+    DPR = 2'd1, // Data/Parameter Register, Pg 19
+    CMDR = 2'd2,// Command Register, Pg 19
+    FSMR = 2'd3 // FSM States Register, Pg 20
 } reg_offset;
 
-
+/* Declare the inputs, outputs, and internal signals required for the I2C bus controller */
 bit  clk;
 bit  rst = 1'b1;
 wire cyc;
@@ -33,7 +39,6 @@ bit [6:0] read_address;
 bit rw;
 // ****************************************************************************
 // Clock generator
-
 always begin : clk_gen
      #5;
      clk = 0;
@@ -42,7 +47,7 @@ always begin : clk_gen
 end
 
 // ****************************************************************************
-// Reset generator
+// Reset generator, start high then set it low after a while
 initial begin : rst_gen
     #113 rst = 0;
 end
@@ -51,58 +56,70 @@ end
 // Monitor Wishbone bus and display transfers in the transcript
 initial begin : wb_monitoring
     $timeformat(-9, 2, " ns", 6);
+    // Waits for the cyc_o (cycle valid output) to be asserted, and exits when it is lowered
+    // It logs the transaction details
     wb_bus.master_monitor(read_address,read_data,rw);
+    // Then when cyc_o is lowered, we display the transaction details as a log
     $display("Transaction at %t ns",$time);
     $display("address from WB_IF: %h",read_address);
     $display("data from WB_IF: %h",read_data);
     $display("write_enable from WB_IF:   %b", rw);
 end
 
-// ****************************************************************************
-// Define the flow of the simulation
-
+// The irq interrupt is an output from the DUT, Pg 17
+// It's active high. It's generated when a byte-level command has been completed
+// and the Interrupt Enable bit (IE) in the Control/Status Register (CSR) is equal to '1'.
+// It can be cleared (reset to '0') by reading CMDR register
+// Read data is a global variable to track the transfers
 task wait4intr ();
+    // Wait for the byte-level command to be completed (signaled by the DUT by irq interrupt pin)
 	wait(irq);
-	//read_data = 0;
+    // Clear the interrupt by reading the CMDR Command Register (takes at least 2 clock cycles)
 	wb_bus.master_read(CMDR,read_data);
-	//while(read_data != 8'b1xxxxxxx ) begin
-	//	wb_bus.master_read(CMDR,read_data);
-	//	if (read_data==8'bx1xxxxxx) begin
-	//		$fatal("CMDR register = %0x which means we are getting NACK", read_data);
-	//	end
-	//end
+    // Commented out: wait until the DON(E) bit of CMDR reads '1'
+	/*while(read_data != 8'b1xxxxxxx ) begin
+		wb_bus.master_read(CMDR,read_data);
+		if (read_data==8'bx1xxxxxx) begin
+			$fatal("CMDR register = %0x which means we are getting NACK", read_data);
+		end
+	end*/
 endtask
 
+// ****************************************************************************
+// Define the test flow of the simulation
+// We use the Wishbone master Bus Functional Model (type wb_if, instance handle called wb_bus)
+// The wb_bus BFM allows us to send commands on higher levels of abstractions
+// And it takes care of the real time signal generation, and each function takes as long as it needs to
 initial begin : test_flow
 	
-	@(negedge rst);
-	repeat(5) @(posedge clk);
+	@(negedge rst);             // Wait for the negative edge of the reset
+	repeat(5) @(posedge clk);   // wait for five positive edges of the clock
 
- 	//Enable the IICMB core after power-up
+ 	// Enable the IICMB core and interrupts
 	wb_bus.master_write(CSR,8'b11xxxxxx);
 
-	//Write byte 0x05 to the DPR. This is the ID of desired I2C bus
+	// Write byte 0x05 to the data/parameter register. This is the ID of desired I2C bus
 	wb_bus.master_write(DPR,8'h05);
 
-	//Write byte “xxxxx110” to the CMDR. This is Set Bus command
+	//Write byte “xxxxx110” to the command register. This is Set Bus command (Pg 7)
 	wb_bus.master_write(CMDR,8'bxxxxx110);
 
-	//Wait for interrupt or until DON bit of CMDR reads '1'
+	//Wait for interrupt to signal that a byte-level command has been completed, and then clear the interrupt
 	wait4intr();
 
 	//Giving start command
 	wb_bus.master_write(CMDR,8'bxxxxx100);
 
-	//Waiting for Interrupt
+	//Wait for interrupt to signal that a byte-level command has been completed, and then clear the interrupt
 	wait4intr();
 
-	//Giving write command & slave address as 0x22
+	//Giving write command & slave address as 0x22?
 	wb_bus.master_write(DPR,8'h44);
 
 	//Giving write command
 	wb_bus.master_write(CMDR,8'bxxxxx001);
 
-	//Wait for interrupt
+	//Wait for interrupt to signal that a byte-level command has been completed, and then clear the interrupt
 	wait4intr();
 
 	//byte to be written
@@ -111,19 +128,20 @@ initial begin : test_flow
 	//Next write command
 	wb_bus.master_write(CMDR,8'bxxxxx001);
 
-	//Wait for interrupt
+	//Wait for interrupt to signal that a byte-level command has been completed, and then clear the interrupt
 	wait4intr();
 
-	//Giving the stop command
+	// Issue a stop command to free the selected I2C bus 
 	wb_bus.master_write(CMDR,8'bxxxxx101);
 	
-	//Wait for interrupt
+	//Wait for interrupt to signal that a byte-level command has been completed, and then clear the interrupt
 	wait4intr();	
 	
 	@(posedge clk) $finish;
 
 end
 
+// Timeout after a certain number of cycles
 initial begin : max_timeout
 	#250000 $display("stop the simulation");
 	$finish;
@@ -131,6 +149,7 @@ end
 
 // ****************************************************************************
 // Instantiate the Wishbone master Bus Functional Model
+// This allows us to drive the real time signals to the DUT with a higher layer of abstraction
 wb_if       #(
       .ADDR_WIDTH(WB_ADDR_WIDTH),
       .DATA_WIDTH(WB_DATA_WIDTH)
@@ -139,13 +158,13 @@ wb_bus (
   // System sigals
   .clk_i(clk),
   .rst_i(rst),
-  // Master signals
+  // Master signals. We are just configuring the master.
   .cyc_o(cyc),
   .stb_o(stb),
   .ack_i(ack),
   .adr_o(adr),
   .we_o(we),
-  // Slave signals
+  // Slave signals. Nothing is connected here yet!
   .cyc_i(),
   .stb_i(),
   .ack_o(),
@@ -161,6 +180,7 @@ assign reg_address = reg_offset'(adr);
 
 // ****************************************************************************
 // Instantiate the DUT - I2C Multi-Bus Controller
+// And connect it to the same nets as our wishbone interface (wb_if) called wb_bus
 \work.iicmb_m_wb(str) #(.g_bus_num(NUM_I2C_BUSSES)) DUT
   (
     // ------------------------------------
--- verification_ip/interface_packages/wb_pkg/src/wb_if.sv
+++ verification_ip/interface_packages/wb_pkg/src/wb_if.sv
@@ -109,13 +109,16 @@ task master_read(
                  output bit [DATA_WIDTH-1:0] data
                  );                                                  
 
+        // Send the read command
         @(posedge clk_i);
-        adr_o <= addr;
-        dat_o <= 'bx;
-        cyc_o <= 1'b1;
-        stb_o <= 1'b1;
-        we_o <= 1'b0;
+        adr_o <= addr;  // Write read address
+        dat_o <= 'bx;   // We are reading so use x to show that we are not driving in this module, but we may be driving in other parts of the design. z means tri-state buffer.
+        cyc_o <= 1'b1;  // Indicate the start of a bus cycle
+        stb_o <= 1'b1;  // Select a slave during the bus cycle
+        we_o <= 1'b0;   // Signal read operation
         @(posedge clk_i);
+        // Waits for the acknowledge input (ack_i) to be asserted, indicating that the slave has 
+        // responded to the read request.
         while (!ack_i) @(posedge clk_i);
         cyc_o <= 1'b0;
         stb_o <= 1'b0;
@@ -124,29 +127,40 @@ task master_read(
         we_o <= 1'b0;
         data = dat_i;
 
-endtask        
+endtask
 
-  // ****************************************************************************
-  // Task: master_monitor
-  // Description: Monitors the master signals and captures the transaction
-  //              details (address, data, and write enable)
-  // ****************************************************************************
-     task master_monitor(
-                   output bit [ADDR_WIDTH-1:0] addr,
-                   output bit [DATA_WIDTH-1:0] data,
-                   output bit we                    
-                  );
-                         
-          while (!cyc_o) @(posedge clk_i);                                                  
-          while (!ack_i) @(posedge clk_i);
-          addr = adr_o;
-          we = we_o;
-          if (we_o) begin
+    /*****************************************************************************
+    Task: master_monitor
+    Description:
+        Monitors the master signals and captures the transaction details (address, data, 
+        and write enable) of the most recent transaction. It waits for the valid bus cycle
+        to be asserted high, so it captures the transaction details of the first and most recent 
+        transaction after this function was called
+        ****************************************************************************/
+    task master_monitor(
+                output bit [ADDR_WIDTH-1:0] addr,
+                output bit [DATA_WIDTH-1:0] data,
+                output bit we                    
+                );
+        // Wait for the cyc_o (cycle valid output) signal to be asserted (i.e., to become high). 
+        // This occurs when the master is beginning a valid bus cycle.
+        while (!cyc_o) @(posedge clk_i);
+        // Wait for the ack_i (acknowledge input) signal to be asserted. This means that the slave 
+        // has acknowledged the current transaction.
+        while (!ack_i) @(posedge clk_i);
+        // Capture the address and write enable
+        addr = adr_o;
+        we = we_o;
+        // If it's a write operation
+        if (we_o) begin
             data = dat_o;
-          end else begin
+        // If it's a read operation
+        end else begin
             data = dat_i;
-          end
-          while (cyc_o) @(posedge clk_i);                                                  
-     endtask 
+        end
+        // Waits for the cyc_o signal to be de-asserted (i.e., to become low), indicating that the 
+        // master has finished the current bus cycle.
+        while (cyc_o) @(posedge clk_i);                                                  
+    endtask 
 
 endinterface
